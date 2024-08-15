450c92d29a86291a2f48e33ad2e629fde2769e2e
docioban <dorinciobanu17@gmail.com>
2024-08-15 22:09:22 +0300

    ex01

--- d01/Squares/Assets/Scripts/Camera.cs
+++ d01/Squares/Assets/Scripts/Camera.cs
@@ -2,48 +2,47 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    [SerializeField] private Player[] Players;
-    private Vector3 offset = new Vector3(0, 0, -10f);
+    [SerializeField] private Player[] _players;
+    private Vector3 offset = new(0, 0, -10f);
     private Vector3 velocity = Vector3.zero;
-    private float smoothTime = 0.25f;
-    private int indexPlayer;
+    private readonly float _smoothTime = 0.25f;
+    private int _indexPlayer;
 
     private void Start()
     {
         ActivatePlayer(0);
     }
 
-
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) && indexPlayer != 0)
+        if (Input.GetKeyDown(KeyCode.Alpha1) && _indexPlayer != 0)
         {
             ActivatePlayer(0);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && indexPlayer != 1)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && _indexPlayer != 1)
         {
             ActivatePlayer(1);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && indexPlayer != 2)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && _indexPlayer != 2)
         {
             ActivatePlayer(2);
         }
 
-        MoveFocus(Players[indexPlayer].transform.position);
+        MoveFocus(_players[_indexPlayer].transform.position);
     }
 
     private void ActivatePlayer(int index)
     {
-        if (index >= 0 && index < Players.Length)
+        if (index >= 0 && index < _players.Length)
         {
-            Players[indexPlayer].Desactivate();
-            indexPlayer = index;
-            Players[indexPlayer].Activate();
+            _players[_indexPlayer].Desactivate();
+            _indexPlayer = index;
+            _players[_indexPlayer].Activate();
         }
     }
 
     private void MoveFocus(Vector3 position)
     {
-        transform.position = Vector3.SmoothDamp(transform.position, position + offset, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, position + offset, ref velocity, _smoothTime);
     }
 }
